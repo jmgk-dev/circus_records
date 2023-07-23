15112f07bf3c0f66fa3837f94d07bf48f5f430c3
Circus Records <LabelAssistant@JK-2.local>
2023-07-23 18:21:03 +0100

    updated imports on models and blocks

--- circus_site_2023/settings/production.py
+++ circus_site_2023/settings/production.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DB_NAME = os.environ['DB_NAME']
 DB_USER = os.environ['DB_USER']
-DB_PASSWORD = os.environ['DJANGO_DB_PASSWORD']
+DB_PASSWORD = os.environ['DB_PASSWORD']
 
 
 DATABASES = {
