3a34a41a48b5e6ae4776cfb7ce5f899e8d1fb00f
Circus Records <LabelAssistant@JK-2.local>
2023-07-23 16:45:26 +0100

    added IP address

--- circus_site_2023/settings/dev.py
+++ circus_site_2023/settings/dev.py
@@ -7,7 +7,7 @@ DEBUG = True
 SECRET_KEY = "REMOVED_SECRET_KEY"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["146.190.150.21"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
