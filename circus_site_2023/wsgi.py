a7e4f02ad8540ed515e9dd13b0f6e8a7352f84df
Circus Records <LabelAssistant@JK-2.local>
2023-07-23 17:38:42 +0100

    updated settings to use production

--- circus_site_2023/wsgi.py
+++ circus_site_2023/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "circus_site_2023.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "circus_site_2023.settings.production")
 
 application = get_wsgi_application()
