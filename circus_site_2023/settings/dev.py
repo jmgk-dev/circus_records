a4d47a302dca9d8901990b8d96504247810c0c23
Circus Records <LabelAssistant@JK-2.local>
2023-07-23 18:48:57 +0100

    back to dev

--- circus_site_2023/settings/dev.py
+++ circus_site_2023/settings/dev.py
@@ -6,12 +6,12 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-7zi+l^q)wq!x@#kcl9+@h*pzueeb&zkxs7snb*x1fxd#y+5p94"
