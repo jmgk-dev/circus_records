8640d85286bea3c643feaf92915ca0a2e4128b69
Circus Records <LabelAssistant@JK-2.local>
2023-07-23 19:10:42 +0100

    updated prod setting again

--- circus_site_2023/settings/production.py
+++ circus_site_2023/settings/production.py
@@ -8,7 +8,7 @@ ALLOWED_HOSTS = ["146.190.150.21"]
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 
-try:
-    from .local import *
-except ImportError:
-    pass
+# try:
+#     from .local import *
+# except ImportError:
+#     pass
