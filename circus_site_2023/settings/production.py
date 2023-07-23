d94c60f2115d0a52f1e81c99cc989aefdee02c5f
Circus Records <LabelAssistant@JK-2.local>
2023-07-23 18:05:41 +0100

    added dotenv

--- circus_site_2023/settings/production.py
+++ circus_site_2023/settings/production.py
@@ -4,14 +4,18 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["146.190.150.21"]
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+DB_NAME = os.environ['DB_NAME']
+DB_USER = os.environ['DB_USER']
 DB_PASSWORD = os.environ['DJANGO_DB_PASSWORD']
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'REMOVED_DB_NAME',
-        'USER': 'REMOVED_DB_USER',
-        'PASSWORD': 'REMOVED_DB_PASSWORD',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
         'HOST': 'localhost',
         'PORT': '',
     }
