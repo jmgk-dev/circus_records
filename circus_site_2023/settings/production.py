de86169b213d3d5cd88bd5befcb05783f583666f
Circus Records <LabelAssistant@JK-2.local>
2023-07-23 18:48:57 +0100

    back to dev

--- circus_site_2023/settings/production.py
+++ circus_site_2023/settings/production.py
@@ -5,21 +5,6 @@ DEBUG = False
 ALLOWED_HOSTS = ["146.190.150.21"]
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-DB_NAME = os.environ['DB_NAME']
-DB_USER = os.environ['DB_USER']
-DB_PASSWORD = os.environ['DB_PASSWORD']
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': DB_NAME,
-        'USER': DB_USER,
-        'PASSWORD': DB_PASSWORD,
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
 
 try:
     from .local import *
