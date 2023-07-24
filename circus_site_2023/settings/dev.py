abbfd4a57c69eac27276be1c6d9832cf901aee5c
Circus Records <LabelAssistant@JK-2.local>
2023-07-24 15:52:49 +0100

    updated settings again

--- circus_site_2023/settings/dev.py
+++ circus_site_2023/settings/dev.py
@@ -1,3 +1,5 @@
+import os
+
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -5,6 +7,17 @@ DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'circrecs',
+        'USER': 'projectuser1',
+        'PASSWORD': 'pocolimon',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # DATABASES = {
 #     "default": {
--- circus_site_2023/settings/production.py
+++ circus_site_2023/settings/production.py
@@ -6,7 +6,24 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["146.190.150.21"]
 
-# try:
-#     from .local import *
-# except ImportError:
-#     pass
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+DB_NAME = os.environ['DB_NAME']
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+try:
+    from .local import *
+except ImportError:
+    pass
