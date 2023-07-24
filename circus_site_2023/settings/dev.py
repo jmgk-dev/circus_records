9e33990f213f180fc629f5a7cc0c46c7c1f0384f
Circus Records <LabelAssistant@JK-2.local>
2023-07-24 15:52:49 +0100

    updated settings again

--- circus_site_2023/settings/dev.py
+++ circus_site_2023/settings/dev.py
@@ -1,3 +1,5 @@
+import os
+
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -5,6 +7,17 @@ DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'REMOVED_DB_NAME',
+        'USER': 'REMOVED_DB_USER',
+        'PASSWORD': 'REMOVED_DB_PASSWORD',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # DATABASES = {
 #     "default": {
