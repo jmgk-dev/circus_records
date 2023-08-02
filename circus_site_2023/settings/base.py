b2dc785502c2592a5b0a1e7821b3b032d8c37689
Circus Records <LabelAssistant@JK-2.local>
2023-08-02 10:34:41 +0100

    updated example domain

--- circus_site_2023/settings/base.py
+++ circus_site_2023/settings/base.py
@@ -172,7 +172,7 @@ WAGTAILSEARCH_BACKENDS = {
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-WAGTAILADMIN_BASE_URL = "http://example.com"
+WAGTAILADMIN_BASE_URL = "http://http://146.190.150.21"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
