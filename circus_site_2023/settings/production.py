8e634e2e2af6b7faaf93893fee071844c795883f
Circus Records <LabelAssistant@JK-2.local>
2023-08-30 15:08:14 +0100

    loads of front end

--- circus_site_2023/settings/production.py
+++ circus_site_2023/settings/production.py
@@ -6,6 +6,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["146.190.150.21"]
 
+WAGTAILADMIN_BASE_URL = "http://146.190.150.21"
+
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DB_NAME = os.environ['DB_NAME']
@@ -21,6 +23,11 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     }
+}
+
+BUGSNAG = {
+    'api_key': '1546c267ada4ec3b888f8b70705cb5ee',
+    'project_root': '/home/jamiek/crsite',
 }
 
 try:
