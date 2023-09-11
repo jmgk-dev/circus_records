a02e6a7eced8a613fff4d5467c962717d68223cd
Circus Records <LabelAssistant@JK-2.local>
2023-09-11 13:23:43 +0100

    requested changes

--- staticfiles/js/releases_slick.js
+++ staticfiles/js/releases_slick.js
@@ -1,51 +1,67 @@
 $('.artist_list').slick({
-    dots: true,
+    dots: false,
     arrows: true,
     infinite: false,
     speed: 300,
     slidesToShow: 5,
     slidesToScroll: 4,
     responsive: [
-      {
-        breakpoint: 1500,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 3,
-          arrows: true,
-        }
-      },
+      // {
+      //   breakpoint: 1500,
+      //   settings: {
+      //     slidesToShow: 4,
+      //     slidesToScroll: 3,
+      //     arrows: true,
+      //   }
+      // },
 
       {
         breakpoint: 1300,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
           slidesToScroll: 3,
           arrows: true,
+          dots: true,
         }
       },
 
       {
-        breakpoint: 1100,
+        breakpoint: 900,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 2,
           arrows: true,
+          dots: true,
+        }
+      },
+
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: true,
+          dots: true,
         }
       },
 
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 1.5,
-          slidesToScroll: 2,
+          slidesToShow: 1.4,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false,
         }
       },
       
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1.13,
+          slidesToShow: 1,
           slidesToScroll: 1,
+          dots: true,
+          arrows: true,
         }
       }
       // You can unslick at a given breakpoint now by adding:
