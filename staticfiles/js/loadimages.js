74541d3f16be7bf41c6ca06b399775f57cd18d00
Circus Records <LabelAssistant@JK-2.local>
2023-10-26 14:45:36 +0100

    front page logo

---
+++ staticfiles/js/loadimages.js
@@ -0,0 +1,6 @@
+// imagesLoaded( document.querySelector('#slideshow-container'), function( instance ) {
+//     console.log('all images are loaded');
+//     document.getElementById("slideshow-container").style.display = "grid";
+//     document.getElementById("content-wrapper").style.display = "none";
+//   });
+
