48f13998e9c00377f48da459bd8f5450d62b55e1
Circus Records <LabelAssistant@JK-2.local>
2023-10-27 11:48:20 +0100

    added loader

--- staticfiles/js/loadimages.js
+++ staticfiles/js/loadimages.js
@@ -1,6 +1,22 @@
-// imagesLoaded( document.querySelector('#slideshow-container'), function( instance ) {
-//     console.log('all images are loaded');
-//     document.getElementById("slideshow-container").style.display = "grid";
-//     document.getElementById("content-wrapper").style.display = "none";
-//   });
+// Get all images on the page
 
+const images = document.querySelectorAll('img');
+
+// Create a counter to track the number of images that have loaded
+
+let loadedImages = 0;
+
+// Add a load event listener to each image
+images.forEach(image => {
+  image.addEventListener('load', () => {
+    // Increment the loaded images counter
+    loadedImages++;
+
+    // If all images have loaded, hide the loading screen
+
+    
+if (loadedImages === images.length) {
+      document.getElementById('loading-screen').style.display = 'none';
+    }
+  });
+});
