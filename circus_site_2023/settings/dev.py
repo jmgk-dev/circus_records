acffd4cacb60c53f6c30eafa26872db7234b1df7
Sonny Baker <sonny@hactar.is>
2023-11-13 23:09:29 +0000

    Fixing prod setup

--- circus_site_2023/settings/dev.py
+++ circus_site_2023/settings/dev.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date, timedelta
 
 from .base import *
 
@@ -9,13 +10,6 @@ WAGTAILADMIN_BASE_URL = "http://127.0.0.1"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
 
 DATABASES = {
     "default": {
