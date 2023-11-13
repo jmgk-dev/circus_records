8de9e046843ae4aecf0ecd14c5ea7946d8474fe9
Sonny Baker <sonny@hactar.is>
2023-11-13 20:58:50 +0000

    Adding Django storages

--- circus_site_2023/settings/production.py
+++ circus_site_2023/settings/production.py
@@ -30,7 +30,18 @@ BUGSNAG = {
     'project_root': '/home/jamiek/crsite',
 }
 
-try:
-    from .local import *
-except ImportError:
-    pass
+AWS_S3_REGION_NAME = "sfo3"
+AWS_STORAGE_BUCKET_NAME = "circus-site-23"
+AWS_S3_ENDPOINT_URL = f"https://{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
+AWS_S3_CUSTOM_DOMAIN = (
+    f"https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_REGION_NAME}.cdn.digitaloceanspaces.com"
+)
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {},
+    },
+}
+
+MEDIA_URL = '{}{}'.format(AWS_S3_CUSTOM_DOMAIN, MEDIA_ROOT)
