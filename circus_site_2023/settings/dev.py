b9205e87348aa589c0cc08e7759cf1e0a44d0245
Sonny Baker <sonny@hactar.is>
2023-11-13 22:10:01 +0000

    Storage settings

--- circus_site_2023/settings/dev.py
+++ circus_site_2023/settings/dev.py
@@ -35,9 +35,3 @@ SECRET_KEY = "REMOVED_SECRET_KEY"
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-
-try:
-    from .local import *
-except ImportError:
-    pass
