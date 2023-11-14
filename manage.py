ebad620ed05c78ca241b6daa666ca30cf9763220
Sonny Baker <sonny@hactar.is>
2023-11-14 10:09:48 +0000

    Working server switch and load dot_env in manage.py

--- manage.py
+++ manage.py
@@ -2,9 +2,13 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 if __name__ == "__main__":
     server_env = os.environ.get('SERVER_ENV', 'dev')
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"circus_site_2023.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"circus_site_2023.settings.{server_env}")
 
     from django.core.management import execute_from_command_line
 
