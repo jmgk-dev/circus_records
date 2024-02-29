e4772f1dd15f43d8dece8f72a2c9532faf31282e
Jamie Kingett <jamie.mgk@gmail.com>
2024-02-29 10:59:02 +0000

    corrected error in urls.py

--- circus_site_2023/urls.py
+++ circus_site_2023/urls.py
@@ -14,7 +14,6 @@ from django.conf.urls import include
 from wagtailautocomplete.urls.admin import urlpatterns as autocomplete_admin_urls
 
 urlpatterns = [
-    path('sitemap.xml', sitemap),
     path("django-admin/", admin.site.urls),
     path("admin/autocomplete/", include(autocomplete_admin_urls)),
     path("admin/", include(wagtailadmin_urls)),
@@ -35,6 +34,7 @@ urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
+    path("sitemap.xml", sitemap),
     path("", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
