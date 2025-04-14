41f0e7a154db808fec84962ff118b9f66ae15427
Jamie Kingett <jamiekingett@Jamies-MacBook-Pro.local>
2025-04-14 15:07:44 +0100

    removed static, updated image chooser

--- circus_site_2023/settings/dev.py
+++ circus_site_2023/settings/dev.py
@@ -1,5 +1,6 @@
 import os
 from datetime import date, timedelta
+from django.core.management.utils import get_random_secret_key
 
 from .base import *
 
@@ -18,8 +19,7 @@ DATABASES = {
     }
 }
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "REMOVED_SECRET_KEY"
+SECRET_KEY = os.getenv('DJANGO_DEV_KEY', get_random_secret_key())
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
