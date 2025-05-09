224291f0de610d96f64769f3066f48bce50dfd53
Jamie Kingett <jamiekingett@Jamies-MacBook-Pro.local>
2025-05-09 15:41:12 +0100

    updated requirements and meta image

--- circus_site_2023/urls.py
+++ circus_site_2023/urls.py
@@ -29,13 +29,4 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
-    path("sitemap.xml", sitemap),
-    path("", include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
-]
+
