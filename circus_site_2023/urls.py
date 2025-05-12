d94b000a675757cfa428de2903c7ecc94cab84a7
Jamie Kingett <jamiekingett@Jamies-MacBook-Pro.local>
2025-05-12 16:04:43 +0100

    added robots.txt

--- circus_site_2023/urls.py
+++ circus_site_2023/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
+from django.http import HttpResponse
 from django.urls import include, path
 from django.contrib import admin
+from django.views.decorators.cache import cache_page
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -12,8 +14,17 @@ from search import views as search_views
 from wagtailautocomplete.urls.admin import urlpatterns as autocomplete_admin_urls
 
 sitemaps = {
-    'pages': WagtailSitemap(),  # Ensure this is an instance, not the class itself
+    'pages': WagtailSitemap(),
 }
+
+@cache_page(60 * 60 * 24)
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Disallow: /admin/",
+        "Sitemap: https://circus-records.co.uk/sitemap.xml",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
@@ -22,6 +33,7 @@ urlpatterns = [
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt", robots_txt, name="robots_txt"),
     path("", include(wagtail_urls)),
 ]
 
